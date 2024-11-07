68bfda08beede9d95ae8b418f02a5a70fc2ed530
Tabitha-Twitchit <tabitha@tabithanikolai.com>
2024-11-07 15:29:06 -0600

    Completed contact list

--- day-4/contact-list.js
+++ day-4/contact-list.js
@@ -23,7 +23,7 @@ E
  *    X 3. findContact(fullName): takes a full-name String, like 'Max Gaudin', and 
  *         returns the contact object if found in the contacts-list, or, 
  *         undefined if the fullName does not match any contacts in the list.
- *      4. removeContact(contact): takes a contact object to be removed from 
+ *    X 4. removeContact(contact): takes a contact object to be removed from 
  *         the contact-list.
  *      5. add a printAllContactNames() Function to your makeContactList() factory. The printAllContactNames() Function should 
  *         return a String formated with all the full-names of the separated 
@@ -103,6 +103,38 @@ function makeContactList() {
         }
         // This is helpful to remember, that if what you have is Apples to Apples objects, you can just use strict compare. 
         // No need to hack up strings or use fancy methods. 
+       },
+       printAllContactNames: function(){
+        // returns a single string comprised of all concatenated first and last names with spacing and line breaks
+        // last name can't have a line break after.
+        // What is a JS line break string literal?
+        
+        // create results string
+        let results = "";
+        // loop the array
+
+        for (let i = 0; i < contacts.length; i++){
+            // seems like to reach into the obj in the array we need a ref to it?
+            let currentObj = contacts[i];
+            // if i != the last array element do the full deal
+            if(i !== contacts.length -1){
+                // access the value of first name, and to the string with a space,
+                //  access the value of last name, add it to the string. Add a line br
+                results += currentObj.nameFirst + " " + currentObj.nameLast + "\n"; 
+            // else if i === array.length
+            } else if (i === contacts.length -1){
+                // do the above stuff withOUT the linebreak.
+                results += currentObj.nameFirst + " " + currentObj.nameLast;
+            }
+        }
+        
+// return the results string  
+console.log(results);
+return results;
+
+
+
+
        }
     }
 }
